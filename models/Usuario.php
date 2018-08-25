922ea921c732491b24d4b13a092d9c22a0ecd928
Jiboli <joao.riboli@outlook.com>
2018-08-24 21:22:17 -0300

    login

--- models/Usuario.php
+++ models/Usuario.php
@@ -14,7 +14,7 @@ use Yii;
  *
  * @property Movimentacoes[] $movimentacoes
  */
-class Usuario extends \yii\db\ActiveRecord
+class Usuario extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
 {
     /**
      * {@inheritdoc}
@@ -56,4 +56,59 @@ class Usuario extends \yii\db\ActiveRecord
     {
         return $this->hasMany(Movimentacoes::className(), ['usuario_id' => 'id']);
     }
+    //HERDADOS
+
+    public static function findIdentity($id)
+    {
+        return static::findOne(['email'=> $id ]);
+    }
+
+      /**
+     * @return string a chave de autenticação do usuário atual
+     */
+    public function getAuthKey()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param string $authKey
+     * @return bool se a chave de autenticação do usuário atual for válida
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->getAuthKey() === $nome;
+    }
+
+    /**
+     * Localiza uma identidade pelo token informado
+     *
+     * @param string $token o token a ser localizado
+     * @return IdentityInterface|null o objeto da identidade que corresponde ao token informado
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return static::findOne(['access_token' => $token]);
+    }
+
+    /**
+     * @return int|string o ID do usuário atual
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function validatePassword($password){
+        return $this->senha ===$password;
+    }
+
+   /* public function login(){
+        $usuBD = Usuario::findIdentity($this->$email);
+       /* if(isset($usuBD) && $usuBD->validatePassword($this->senha)){
+            return Yii::app->user->login($usuBD);
+
+        }
+        return false;
+}*/
 }
